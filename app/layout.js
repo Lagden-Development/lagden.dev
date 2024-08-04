def65eb15ef8f2b72836028c384536560e03a892
Zach Lagden <zach@zachlagden.uk>
2024-08-04 23:11:32 +0000

    📝 Updated metadata.

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,34 @@ import { GeistMono } from "geist/font/mono";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export const metadata = {
+  charset: "UTF-8",
+  themeColor: "#FFFFFF",
   title: "lagden.dev",
-  description: "Welcome to Lagden.dev",
+  description: "A small development group passionate about open-source.",
+  keywords: "lagden, development, open-source, dev",
+  robots: "index, follow",
+  viewport: "width=device-width, initial-scale=1.0",
+  httpEquiv: {
+    "Content-Type": "text/html; charset=utf-8",
+  },
+  language: "English",
+  revisitAfter: "7 days",
+  author: "Lagden Development",
+  og: {
+    type: "website",
+    locale: "en_GB",
+    url: "https://lagden.dev/",
+    title: "lagden.dev",
+    description: "A small development group passionate about open-source.",
+    image: "https://i.lagden.dev/logo.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    url: "https://lagden.dev/",
+    title: "lagden.dev",
+    description: "A small development group passionate about open-source.",
+    image: "https://i.lagden.dev/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
